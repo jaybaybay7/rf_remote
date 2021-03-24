e17514eb4324058e5219bf6db68dbbeabbac04ee
jaybaybay7 <jaymaglione7@gmail.com>
2021-03-23 19:35:12 -0700

    receive.py first commit

---
+++ receive.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(16, GPIO.IN, initial=GPIO.LOW)
+GPIO.setup(36,GPIO.OUT, initial=GPIO.LOW)
+
+while True:
+    print(GPIO.input(16))
+    if(GPIO.input(16)== 1):
+        GPIO.output(36,1)
+        
+    time.sleep(1)
+
+
