289861eb7301a38b37f113cc682490dcbfa61087
jaybaybay7 <jaymaglione7@gmail.com>
2021-03-24 17:35:10 -0700

    realizing I don't need delay logic

--- receive.py
+++ receive.py
@@ -10,9 +10,6 @@ while True:
     print(GPIO.input(16))
     if(GPIO.input(16)== 1):
         GPIO.output(36,1)
-        if(GPIO.input(16) == 0):
-            time.sleep(5)
-            GPIO.output(36,0)
     time.sleep(1)
 
 
